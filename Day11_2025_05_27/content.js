cfa12dfa9d0edf0414f96f94e1d9f427d14cee60
daoxuanhoang <hoangdx.ptit@gmail.com>
2025-09-04 14:46:34 +0700

    up

--- Day11_2025_05_27/content.js
+++ Day11_2025_05_27/content.js
@@ -1,4 +1,4 @@
-const title = document.querySelector('h3 > a');
+const title = document.querySelector(`div.${title___2T7sN}`);
 if (title) {
-    title.textContent = 'Sinh viên UDU'
+    title.textContent = 'Thông báo: Toàn bộ sinh viên D24 ngành Cử nhân CNTT định hướng ứng dụng - UDU bắt đầu từ HK1 năm học 2025 - 2026 sẽ chuyển cơ sở giảng dạy sang Trụ sở chính Hoàng Quốc Việt'
 }
